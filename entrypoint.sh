08d2d511430cf42d37eef3d1cdc7593feea22c5c
Gabriel Ferreira <ferr3ira.gabriel@gmail.com>
2024-01-19 12:02:23 -0300

    First commit

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+echo ${GITHUB_REF#refs/heads/}
+echo ${GITHUB_SHA::7}
+echo ${GITHUB_REF#refs/*/}
+
+REF=${GITHUB_REF#refs/heads/}
+COMMIT_ID=${GITHUB_SHA::7}
+
+if [[ "$REF" == "develop" ]]; then
+    printf "Branch name: $REF \nCommit ID: $COMMIT_ID"
+    echo "RELEASE_VERSION=$COMMIT_ID" >> $GITHUB_ENV
+    echo "GITOPS_BRANCH=develop" >> $GITHUB_ENV
+
+elif [[ "$REF" == "main" ]]; then
+    printf "Branch name: $REF \nCommit ID: $COMMIT_ID"
+    echo "RELEASE_VERSION=$COMMIT_ID" >> $GITHUB_ENV
+    echo "GITOPS_BRANCH=main" >> $GITHUB_ENV
+
+fi
