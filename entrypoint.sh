eb844163d3266f358ff29b0872b5b32468625a18
Gabriel Ferreira <ferr3ira.gabriel@gmail.com>
2024-03-27 23:27:47 -0300

    development env

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,10 @@ echo ${GITHUB_REF#refs/*/}
 REF=${GITHUB_REF#refs/heads/}
 COMMIT_ID=${GITHUB_SHA::7}
 
-if [[ "$REF" == "develop" ]]; then
+if [[ "$REF" == "development" ]]; then
     printf "Branch name: $REF \nCommit ID: $COMMIT_ID"
     echo "RELEASE_VERSION=$COMMIT_ID" >> $GITHUB_ENV
-    echo "GITOPS_BRANCH=develop" >> $GITHUB_ENV
-
-elif [[ "$REF" == "control-plane-dev" ]]; then
-    printf "Branch name: $REF \nCommit ID: $COMMIT_ID"
-    echo "RELEASE_VERSION=$COMMIT_ID" >> $GITHUB_ENV
-    echo "GITOPS_BRANCH=control-plane-dev" >> $GITHUB_ENV
+    echo "GITOPS_BRANCH=development" >> $GITHUB_ENV
 
 elif [[ "$REF" == "main" ]]; then
     printf "Branch name: $REF \nCommit ID: $COMMIT_ID"
